c7bc43aa108171d1534cccc9a2c56cb13873ddbd
Matin Rusydan <matin.rusydan@gmail.com>
2026-04-29 22:23:48 +0700

    fix error auth admin

--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,12 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
-  const token = request.cookies.get('token')?.value
+  const token = request.cookies.get('admin_session')?.value
 
-  // Biarkan halaman login terbuka
   if (pathname.startsWith('/admin/login')) {
     return NextResponse.next()
   }
 
-  // Proteksi halaman admin lain
   if (pathname.startsWith('/admin')) {
     if (!token) {
       return NextResponse.redirect(new URL('/admin/login', request.url))
